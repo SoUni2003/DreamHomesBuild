341048ed4dedcf9eaa964b3181592d9e2abf60d7
Quoc Nghi Tr <nghik720177@gmail.com>
2024-08-19 09:52:44 +0700

    setup_evr

---
+++ fontEnd/DreamHomesBuild/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./user/pages/footer/footer.component";
+import { HeaderComponent } from "./user/pages/header/header.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'DreamHomesBuild';
+}
