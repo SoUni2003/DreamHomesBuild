0b4362d1741b62b538a066d36dff23069835aa60
Quoc Nghi Tr <nghik720177@gmail.com>
2024-09-14 21:13:45 +0700

    add section home

--- fontEnd/DreamHomesBuild/src/app/app.routes.ts
+++ fontEnd/DreamHomesBuild/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './user/pages/home/home.component';
 import { VechungtoiComponent } from './user/pages/vechungtoi/vechungtoi.component';
 import { ThongtinComponent } from './user/pages/thongtin/thongtin.component';
+import { KhacComponent } from './user/pages/khac/khac.component';
 
 export const routes: Routes = [
     {
@@ -15,8 +16,13 @@ export const routes: Routes = [
     {
         path: 'vechungtoi',
         component: VechungtoiComponent
-    },{
+    },
+    {
         path: 'thongtin',
         component: ThongtinComponent
+    },
+    {
+        path: 'khac',
+        component: KhacComponent
     }
 ];
